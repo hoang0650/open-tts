57ba9509664d922231d135f95662ec0c3869d6d9
hoang0650 <hoangphan29091999@gmail.com>
2026-03-17 21:37:25 +0700

    vv

--- src/train.py
+++ src/train.py
@@ -92,11 +92,19 @@ class TTSDataCollator:
 # 6. Huấn luyện
 training_args = TrainingArguments(
     output_dir="./mms-tts-vie-finetuned",
-    per_device_train_batch_size=16,
-    gradient_accumulation_steps=2,
+    per_device_train_batch_size=8,
+    gradient_accumulation_steps=4,
     learning_rate=2e-5,
     max_steps=10000,
-    fp16=torch.cuda.is_available(),
+    # --- TỐI ƯU HÓA PHẦN CỨNG RTX 4090 ---
+    bf16=True,                          
+    optim="adamw_torch_fused",          # Giờ sẽ chạy ổn định sau khi đã ép kiểu model
+    gradient_checkpointing=True,        # Tiết kiệm VRAM cực lớn
+    dataloader_num_workers=4,           
+    
+    # --- CẤU HÌNH KHÁC ---
+    logging_steps=10,
+    save_steps=1000,
     push_to_hub=True,
     hub_model_id="phgrouptechs/tts-vie-infore",
     report_to="none"
